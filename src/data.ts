059e53cf8fe4ee68ed72ebc0fea50656ceb7d181
rleivap <informaticaredlam@gmail.com>
2023-01-26 17:40:19 -0500

    cambios en las rutas de videos

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,6 @@
 export const tiktoks:any[]=[
-    {id:1, nombre:'CENTRO DE SALUD MENTAL COMUNITARIO TUMI DE ORO', participantes:'Personal de C.S Salud del Tumi', video:"-BnJTCwJYIU"},
-    {id:2, nombre:'PUESTO DE SALUD LAS PAMPAS', participantes:'Personal de Salud de P.S las Pampas', video:"DfiwcRGIRbQ"},
-    {id:3, nombre:'CENTRO DE SALUD SAN MARTIN',participantes:'Personal de Salud C.S de San Martin' ,video:"rlNPzZ_edpw"}
+    {id:1, nombre:'CENTRO DE SALUD MENTAL COMUNITARIO TUMI DE ORO', participantes:'Personal de C.S Salud del Tumi', video:"KW4zvxoDHZQ"},
+    {id:2, nombre:'PUESTO DE SALUD LAS PAMPAS', participantes:'Personal de Salud de P.S las Pampas', video:"R2AESl8BzGQ"},
+    {id:3, nombre:'CENTRO DE SALUD SAN MARTIN',participantes:'Personal de Salud C.S de San Martin' ,video:"H1Evd97NaKo"}
   ]
 
